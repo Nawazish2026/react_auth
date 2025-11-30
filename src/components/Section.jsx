12a4570e35ff7da313aa1a7d275fcd9ecf68a2d9
Nawazish2026 <nawazishhassan2003@gmail.com>
2025-11-30 12:32:59 +0530

    Refactor Section component
    Updated the Section component to use Navbar.module.css for styling and reorganized the JSX structure for better layout.

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,15 +1,25 @@
-import styles from './Section.module.css'
+import styles from './Navbar.module.css'; // Make sure you import Navbar.module.css, NOT Section.module.css if you want the good styling
 
-export default function Section(){
-  return(
+export default function Section() {
+  return (
     <nav className={styles.navbar}>
-          <ul>
-            <li>Home</li>
-            <li>Series</li>
-            <h3 className={styles.logo}>playN</h3>
-            <li>Movies</li>
-            <li>Settings</li>
-          </ul>
-          </nav>
+      
+      {/* Left Side Links */}
+      <ul>
+        <li>Home</li>
+        <li>Series</li>
+      </ul>
+
+      {/* Center Logo */}
+      <div className={styles.logo}>playN</div>
+
+      {/* Right Side Links */}
+      <ul>
+        <li>Movies</li>
+        <li>Settings</li>
+      </ul>
+      
+      {/* Search/Profile would go here if you add the .right div back */}
+    </nav>
   );
 }
